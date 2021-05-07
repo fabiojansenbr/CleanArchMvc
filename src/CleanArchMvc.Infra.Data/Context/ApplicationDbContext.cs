54f133d8f464c3a99de30430c1bc83af254e032c
Fábio Jansen <fabiojansen@outlook.com>
2021-05-07 10:43:41 -0300

    Creating DbContext

---
+++ src/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
