8ae66b0b0493aaefd54e8ff9877c2b2e03698f0a
Fábio Jansen <fabiojansen@outlook.com>
2021-05-10 10:42:16 -0300

    Controllers e views Categories e Controllers

---
+++ src/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        protected readonly ICategoryService _categoryservice;
+
+        public CategoriesController(ICategoryService categoryservice)
+        {
+            _categoryservice = categoryservice;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryservice.GetCategories();
+            return View(result);
+        }
+    }
+}
